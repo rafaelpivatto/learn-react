cb41abe6e692d0846870c816319be8bfe45fe94c
Rafael Pivatto <rafael.pivatto@gmail.com>
2019-08-06 23:49:17 -0300

    add backend

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes')
+const server = express()
+
+mongoose.connect(process.env.MONGO_URL, { 
+    useNewUrlParser: true 
+})
+
+server.use(cors())
+server.use(express.json())
+server.use(routes)
+
+server.listen(3333)
